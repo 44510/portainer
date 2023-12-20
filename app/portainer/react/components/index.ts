41210ba43cdb54d011d91657dea9665a0efeacc1
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-12-20 20:48:31 +0200

    refactor(activity-logs): migrate activity logs table to react [EE-4714]
    refactor(activity-logs): remove old table
    
    fix(activity-logs): fix CE values

--- app/portainer/react/components/index.ts
+++ app/portainer/react/components/index.ts
@@ -45,6 +45,7 @@ import { accessControlModule } from './access-control';
 import { environmentsModule } from './environments';
 import { registriesModule } from './registries';
 import { accountModule } from './account';
+import { activityLogsModule } from './activity-logs';
 
 export const ngModule = angular
   .module('portainer.app.react.components', [
@@ -55,6 +56,7 @@ export const ngModule = angular
     registriesModule,
     settingsModule,
     accountModule,
+    activityLogsModule,
   ])
   .component(
     'tagSelector',
